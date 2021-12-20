7e7035c6948decbe271a227fa461a9f84a4d902d
hexaflexahexagon <hexaflexahexagon@users.noreply.github.com>
2021-12-20 15:56:23 -0500

    feat: day01 part2
    also added checklist to README.md, and checked day1 off

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # AOC2021
 
-My collection of code for AOC 2021, which may or may not be complete. YMMV. 
+My collection of code for AOC 2021, which may or may not be complete. YMMV.
+
+## Checklist
+[X] day 1 
+[ ] day 2 
+[ ] day 3
+[ ] day 4
+[ ] day 5
+[ ] day 6
+[ ] day 7
+[ ] day 8
+[ ] day 9
+[ ] day 10
+[ ] day 11
+[ ] day 12
+[ ] day 13
+[ ] day 14
+[ ] day 15
+[ ] day 16
+[ ] day 17
+[ ] day 18
+[ ] day 19
+[ ] day 20 
+[ ] day 21
+[ ] day 23
+[ ] day 24
+[ ] day 25
--- day01/day01.sh
+++ day01/day01.sh
@@ -1,22 +1,43 @@
 #!/bin/bash
 
-input='inputsmall.txt'
+input='input.txt'
 
-lineOld=""
+lineOld=0
+lineOldOld=0
+
+value=0
+valueOld=0
+
 count=0
+iteration=0
 
 while read -r line; do
-	echo -n "$line "
 	
-	if [[ $lineOld == "" ]]; then
+	# Do a 2 small loops to set up 'Old' line variables
+	if [ $iteration -lt 2 ]; then
+	
+		lineOldOld=$lineOld
+		lineOld=$line
+		
+		value=$(( $line + $lineOld + $lineOldOld ))
+		
+		iteration=$((iteration + 1))
+		continue
+	fi
+
+	value=$(( $line + $lineOld + $lineOldOld ))
+
+	echo -n "$value "
+
+	if [ $valueOld -eq 0 ]; then
 		# This is iteration #1, do nothing
 		echo "(N/A)"
 	else
-		if [ $lineOld -lt $line ]; then
+		if [ $valueOld -lt $value ]; then
 			# We increased
 			count=$((count+1))
 			echo "(Increase)"
-		elif [ $lineOld -gt $line ]; then
+		elif [ $valueOld -gt $value ]; then
 			# We decreased 
 			echo "(Decrease)"
 		else
@@ -24,7 +45,11 @@ while read -r line; do
 			echo "(Equal)"
 		fi
 	fi
-	lineOld="$line"
+	lineOldOld=$lineOld
+	lineOld=$line
+	
+	valueOld=$value
+	value=$(( $line + $lineOld + $lineOldOld ))
 
 done < $input
 
